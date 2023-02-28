f1cb2bca38c24e702a993a1208a6a5aa2a483bc7
zubair2001 <zubair_mansuri2001@outlook.com>
2023-02-28 22:47:19 +0500

    5

--- RunGroupWebApp/Interfaces/IRaceRepository.cs
+++ RunGroupWebApp/Interfaces/IRaceRepository.cs
@@ -7,6 +7,7 @@ namespace RunGroupWebApp.Interfaces
         Task<IEnumerable<Race>> GetAll();
         Task<Race> GetByIdAsync(int id);
         Task<IEnumerable<Race>> GetAllRacesByCity(string city);
+        Task<Race> GetByIdAsyncNoTracking(int id);
         bool Add(Race race);
         bool Update(Race race);
         bool Delete(Race race);
